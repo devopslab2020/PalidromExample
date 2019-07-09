0a12ccab60b878ae7653d1c0422a2edd934f32f3
reecemcg <36105656+reecemcg@users.noreply.github.com>
2019-07-09 16:26:32 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalidromeExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPalidrome("Deleveled"));
+            Console.ReadLine();
+        }
+
+        public static bool IsPalidrome(string word)
+        {
+            string originalWord = word.ToLower();
+            char[] wordArray = word.ToLower().ToCharArray();
+            Array.Reverse(wordArray);
+
+            string reversedWord = new string(wordArray);
+
+            if (originalWord.Equals(reversedWord))
+                return true;
+            else
+                return false;
+        }
+    }
+}
